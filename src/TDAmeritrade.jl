92bb6261ad25fe6c0a983129e02933f62d30726e
Moelf <proton@jling.dev>
2020-08-03 19:56:55 -0400

    interactive auth done

--- src/TDAmeritrade.jl
+++ src/TDAmeritrade.jl
@@ -1,7 +1,7 @@
-#ntvwyo1fZelbFQPPqwt8N1LBZ7xi8GLwl%2BMJHZPZRhOM6Jdg0ieIrANAOFWVaKYX%2FjhUo5NcVVeEY24ZH01zEqlLzjsPsYtco48TuSEsNEFWx%2Flb6GxLG5zMy2tjHTi%2FvOHDIW2BrNpfkzqVZOPpke3F%2BqBETRIxJvjzHQbCaMCkd9ym%2BOdpOLzhOqbTtQyyERrvbypKfKuTvuEHq1T1qjM1yhLWrzkVkEKUj6n4XGXsr36nZC18n0ybVQFY4dMSA1%2BrEzEO0CCFyuqOXGTuP70IbXX6T8RpvQQU2ScCsd%2B0wW%2Fi8%2FwfGWmfGLnktkQrJ2zeC00IkbctNnhZmVMibOJylcQekcJECpLjJXCTcdWHPvzuACayrVT5ezPmesFyKHZCmDVLVj4BlrdxOm4n%2FkMA1HKYJ5r%2FBYSoPKDVuUbOPvu73kOe8qdqcNs100MQuG4LYrgoVi%2FJHHvl7pUvsiMTETvU5PeSpkVcCivi9sEYYmTLOQNaXxQ8izrs2SAlQw0cfiGtoGp6s8U99sgsjzombJEGgJnbcIGXKNJQh6aHrCU%2F3d6UclcAQ3Ydg4gV%2B6lu9msxTmk1uAvyjzwcaPos5%2BitPu%2FrZeGW3MrRiNLSS3gO3vIkxJsGxQZr29A30V56Hsw%2FNAjrp1Dd0aF7oCVqSj%2BeBOmuzqTZnQu2r4bseA767%2B%2BiMBeQwXODhKA97K4sJCu8ciku8e4KFc7kLCwphXrbW8VvJCYaFYrfXcyYYHemLSKxJRzPqfMj9lOXE7LO7kFqgFf8YQT0OzOuTZIWsbGhyI%2FuBUj24xlK2HWMPkh8Q0ud0%2BOqRxg2sLTgiKelRBqftjQACknO%2BSrr05nmmaqa80hcE8bn1tTxHA3YakYtyyBoOaU6qICX5NV5sgIlPkZjxzk%3D212FD3x19z9sWBHDJACbC00B75E
 module TDAmeritrade
 
-using HTTP
+using HTTP 
+using JSON3
 
 include("auth.jl")
 include("price_history.jl")
@@ -24,27 +24,5 @@ mutable struct AUTH_KEYS{T<:AbstractString}
     AUTH_KEYS() = new{String}("", "", "", "","http://localhost")
 end
 
-
 AUTH_KEY = AUTH_KEYS()
-AUTH_KEY.CONSUMER_KEY = try
-    ENV["JL_TD_CONSUMER_KEY"]
-catch
-    error("Key environmen variable JL_TD_CONSUMER_KEY not found")
-end
-
-AUTH_KEY.CODE = try
-    ENV["JL_TD_CODE"]
-catch
-    @info "first time auth needs manual invervention"
-    ""
-end
-
-if AUTH_KEY.CODE |> isempty
-    @info initial_auth()
-    @info "paste code=<blah> from address bar to here"
-    print("code:")
-    AUTH_KEY.CODE = readline() |> HTTP.URIs.unescapeuri
-end
-
-
 end
--- src/auth.jl
+++ src/auth.jl
@@ -1,23 +1,59 @@
 function initial_auth()
     @info "open the url in browser and copy the apikey back"
     auth_url = HTTP.URI(
-    host="auth.tdameritrade.com",
-    scheme="https",
-    path="/auth",
-    query = Dict("response_type" => "code", 
+        host="auth.tdameritrade.com",
+        scheme="https",
+        path="/auth",
+        query = [
+                 "response_type" => "code", 
                  "redirect_uri" => AUTH_KEY.CALLBACK_URI,
                  "client_id"=>"$(AUTH_KEY.CONSUMER_KEY)@AMER.OAUTHAP"
-                 )
+                ]
    )
 end
 
 function access_token(code)
-    url = construct_api("oauth2/token",
-                        Dict("grant_type" => "authorization_code",
-                             "access_type" => "offline",
-                             "code" => code,
-                             "client_id" => AUTH_KEY.CONSUMER_KEY,
-                             "redirect_uri" => AUTH_KEY.CALLBACK_URI
-                            )
-                       )
+    url = "https://api.tdameritrade.com/v1/oauth2/token"
+    params = HTTP.escape([
+              "grant_type" => "authorization_code",
+              "refresh_token" => "",
+              "access_type" => "offline",
+              "code" => code,
+              "client_id" => AUTH_KEY.CONSUMER_KEY,
+              "redirect_uri" => AUTH_KEY.CALLBACK_URI
+             ])
+    json_result = HTTP.post(
+                            url,
+                ["Content-Type"=>"application/x-www-form-urlencoded"],
+                params
+               ).body |> String |> JSON3.read
+    json_result
+end
+
+function inter_auth(KEY=AUTH_KEY)
+    KEY.CONSUMER_KEY = try
+        ENV["JL_TD_CONSUMER_KEY"]
+    catch
+        error("Key environmen variable JL_TD_CONSUMER_KEY not found")
+    end
+
+    KEY.CODE = try
+        ENV["JL_TD_CODE"]
+    catch
+        @info "first time auth needs manual invervention"
+        ""
+    end
+
+    if KEY.CODE |> isempty
+        @info initial_auth()
+        @info "paste code=<blah> from address bar to here"
+        print("code:")
+        KEY.CODE = readline() |> HTTP.URIs.unescapeuri
+    end
+
+    token = access_token(KEY.CODE)
+    KEY.ACCESS_TOKEN, KEY.REFRESH_TOKEN = token[:access_token], token[:refresh_token]
+
+    @info "interactive auth done"
+    nothing
 end
